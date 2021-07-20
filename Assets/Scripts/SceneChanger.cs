9e95406768b4ad8bc98477c5e59b654bf2731886
WCallihan <willcallihan13@gmail.com>
2021-07-20 09:41:30 -0500

    Level 1 ending cutscene complete and integrated with Level 1.

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,10 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class SceneChanger : MonoBehaviour {
 
-    public void StartLevel1() {
-        SceneManager.LoadScene("Level 1");
+    [SerializeField] string nextScene = "";
+    [SerializeField] PlayableDirector endLevelTimeline;
+
+    //triggers when the player crosses the level changing threshold
+    private void OnTriggerEnter2D(Collider2D collision) {
+        EndScene();
+    }
+
+    //called by the trigger collider at the end of the level
+    public void EndScene() {
+        if(endLevelTimeline != null) {
+            endLevelTimeline.Play();
+        } else {
+            StartNextScene();
+        }
+    }
+
+    //called at the end of the fade out timeline or by EndScene if there is no fade out
+    public void StartNextScene() {
+        if(nextScene != "") {
+            SceneManager.LoadScene(nextScene);
+        } else {
+            Debug.Log("No Next Scene Given");
+        }
     }
 }
